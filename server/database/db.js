d52a14d95cf73852315ddf815101ab52935be959
Siddhant Goyal <g.siddhant@iitg.ac.in>
2024-02-22 10:43:25 +0530

    Project updated

---
+++ server/database/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const DBConnection = async () => {
+
+    
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
